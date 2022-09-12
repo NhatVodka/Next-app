89fe0ef34837041b7311ec116f82ca71f1727254
nhatbk <nhatbk@vmodev.com>
2022-09-12 14:25:40 +0700

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
-import EvenList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
 
-export default function HomePage() {
-  const featureEvents = getFeaturedEvents();
-  console.log(featureEvents);
+export default function HomePage(props) {
+  const { products } = props;
+
   return (
-    <div>
-      <EvenList items={featureEvents}></EvenList>
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
 }
